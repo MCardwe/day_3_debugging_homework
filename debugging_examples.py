ffaf68186d73fdb27ca7ffce839e615bddb4c5dc
Max Cardwell <maximillian.cardwell@gmail.com>
2022-02-23 19:36:20 +0000

    Homework complete

---
+++ debugging_examples.py
@@ -0,0 +1,27 @@
+# my_string = "This is a test string"
+
+# string_length = len(my_string)
+
+# print(string_length)
+
+#--------
+
+# my_list = [1, 2, 3, 4]
+
+# print(my_list + 4)
+
+#----------
+
+# def add(a, b):
+#   return a + b
+
+# print(add(4, 5))
+
+#---------
+
+# my_dict = {
+#   "name": "Beth",
+#   "age" : 25
+# }
+
+# numbers = [1, 2, 3]
